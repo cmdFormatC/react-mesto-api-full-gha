3e24b512fbd5bb581c4f50d1bbd7fc1e582edb30
cmdFormatC <117574451+cmdFormatC@users.noreply.github.com>
2024-02-22 22:29:55 +0300

    review fixes3

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -98,6 +98,7 @@ function App() {
   };
 
   function handleCardLike(card) {
+    console.log(currentUser._id);
     const isLiked = card.likes.some(i => i._id === currentUser._id);
     console.log(card._id);
     console.log(isLiked);
