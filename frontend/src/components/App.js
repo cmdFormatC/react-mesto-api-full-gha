7759528279c247eb2650e662f242387556ba9f55
cmdFormatC <117574451+cmdFormatC@users.noreply.github.com>
2024-02-22 22:36:51 +0300

    review fixes5

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -98,10 +98,7 @@ function App() {
   };
 
   function handleCardLike(card) {
-    console.log(currentUser._id);
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
-    console.log(card._id);
-    console.log(isLiked);
+    const isLiked = card.likes.some(i => i === currentUser._id);
     api.togglelike(card._id, !isLiked)
     .then((newCard) => {
       console.log(newCard)
